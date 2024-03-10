b73fd55e20e95769c758ce0051b2673aeabf2b48
MrAndreyOfficial <mrandreyofficial@gmail.com>
2024-03-10 23:43:12 +0500

    Library for strings

---
+++ C#/String Library/Readme.md
@@ -0,0 +1,9 @@
+This library contains extension methods for `strings`.
+
+This library is located in the `String_Library` namespace.
+
+Methods:
+1. Append(`string` value) returns `string`
+2. Remove(`int` startIndex, `int` endIndex) returns `string`
+3. GetFirstChar returns `char`
+4. GetLastChar returns `char`
---
+++ C#/String Library/StringExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+
+namespace String_Library;
+
+public static class StringExtensions
+{
+    public static string Append(this string str, string value)
+    {
+        var stringBuilder = new StringBuilder(str);
+
+        stringBuilder.Append(value);
+
+        return stringBuilder.ToString();
+    }
+
+    public static string Remove(this string str, int startIndex, int endIndex)
+    {
+        var stringBuilder = new StringBuilder(str);
+
+        stringBuilder.Remove(startIndex, endIndex);
+
+        return stringBuilder.ToString();
+    }
+
+    public static char GetFirstChar(this string str) => str[0];
+    public static char GetLastChar(this string str) => str[^1];
+}
